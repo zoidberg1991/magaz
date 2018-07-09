8f336eea04b217613c01e6f1c992ec5a43ffcdad
Zoidberg <dima-marenin@yandex.ru>
2018-07-09 15:50:40 +0300

    delet magaz

--- magaz/mysite/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-from django.conf import settings
-# Основыне настройки Django для celery
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-app = Celery('mysite')
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
